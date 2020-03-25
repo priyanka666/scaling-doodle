d33382924a92a8134a457a4a3bb862b94d679f81
mohith166 <mohith.devathi@gmail.com>
2020-03-26 00:37:08 +0530

    Update member_class.py

--- member_class.py
+++ member_class.py
@@ -1,3 +1,6 @@
+from constants import GENDER_MALE
+from constants import GENDER_FEMALE
+
 
 class Member(object):
     def __init__(self, founder, gender):
@@ -6,7 +9,7 @@ class Member(object):
         Initializes a member.
         Name is the string of name of this node,
         gender specifies the gender of the member
-        parent is None, and no children
+        mother, father, spouse are None, and no children
         """
         self.name = founder
         self.gender = gender
@@ -14,67 +17,41 @@ class Member(object):
         self.father = None
         self.spouse = None
         self.children = []
+        self.siblings = []
 
     def __str__(self):
         return self.name
 
-    def add_mother(self, mother):
-        """
-        mother: Member
-        Sets the parent of this node to the `mother` Member node
-        """
-        self.mother = mother
-
-    def add_father(self, father):
-        """
-        mother: Member
-        Sets the parent of this node to the `mother` Member node
-        """
-        self.father = father
-
-    def get_mother(self):
-        """
-        Returns the parent Member node of this Member
-        """
-        return self.mother
-
-    def get_father(self):
-        """
-        Returns the parent Member node of this Member
-        """
-        return self.father
-
-    def get_parent(self):
-        """
-        Returns the parent Member node of this Member
-        """
-        return self.father or self.mother
-
     def can_add_child(self):
-        return self.gender == 'Female'
+        return self.gender == GENDER_FEMALE
 
-    def add_spouse(self, spouse):
-        self.spouse = spouse
-
-    def is_parent(self, mother):
-        """
-        mother: Member
-        Returns: Boolean, whether or not `mother` is the
-        parent of this Member
-        """
-        return self.parent == mother
-
-    def add_child(self, child):
+    def add_child(self, new_child):
         """
         child: Member
         Adds another child Member node to this Member
         """
-        self.children.append(child)
+        # adding new child to siblings of the parent children
+        new_child.mother = self
+        new_child.father = self.spouse
+        new_child.siblings += self.children
 
-    def is_child(self, child):
-        """
-        child: Member
-        Returns: Boolean, whether or not `child` is a
-        child of this Member
-        """
-        return child in self.children
+        for child in self.children:
+            child.siblings.append(new_child)
+
+        self.children.append(new_child)
+        self.spouse.children.append(new_child)
+
+    def get_son(self):
+        return [child for child in self.children if child.gender == GENDER_MALE]
+
+    def get_daughter(self):
+        return [child for child in self.children if child.gender == GENDER_FEMALE]
+
+    def get_siblings(self):
+        return self.siblings
+
+    def get_brothers(self):
+        return [sibling for sibling in self.siblings if sibling.gender == GENDER_MALE]
+
+    def get_sisters(self):
+        return [sibling for sibling in self.siblings if sibling.gender == GENDER_FEMALE]
